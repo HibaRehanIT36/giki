1bd882cc76196b0b305bc34602da0e8f27b2fbda
HibaRehanIT36 <hibarehan36@gmail.com>
2023-12-16 15:22:50 +0300

    create image provider file

---
+++ src/providers/imageProvider.ts
@@ -0,0 +1,27 @@
+import logoImage from "../assets/logo.png";
+import letterImage from "../assets/A.png";
+import penImage from "../assets/penIcon.png";
+import checkImage from "../assets/checkIcon.png";
+import innovationImage from "../assets/1.png";
+import medicineImage from "../assets/2.png";
+import educationImage from "../assets/3.png";
+import pharmechImage from "../assets/4.png";
+import researchImage from "../assets/5.jpg";
+import technologyImage from "../assets/6.png";
+import telemediceneImage from "../assets/7.png";
+import loremIpsumImage from "../assets/8.png";
+import successImage from "../assets/successImage.png";
+
+export const logoImageeSrc: string = logoImage;
+export const letterImageSrc: string = letterImage;
+export const penImageSrc: string = penImage;
+export const checkImageSrc: string = checkImage;
+export const innovationImageSrc: string = innovationImage;
+export const medicineImageSrc: string = medicineImage;
+export const educationImageSrc: string = educationImage;
+export const pharmechImageSrc: string = pharmechImage;
+export const researchImageSrc: string = researchImage;
+export const technologyImageSrc: string = technologyImage;
+export const telemediceneImageSrc: string = telemediceneImage;
+export const loremIpsumImageSrc: string = loremIpsumImage;
+export const successImageSrc: string = successImage;
